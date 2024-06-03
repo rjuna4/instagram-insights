98023d4562a2e15e4a6709d4505fd40d8abb99ec
rjunagadhwala <rjunagadhwala@luc.edu>
2024-06-02 19:34:37 -0500

    Added packages, global styles and Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import './CSS/App.css';
+import React from "react";
+import Navbar from "./Components/Navbar";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import AppProjects from "./Pages/AppProjects";
+import WebProjects from "./Pages/WebProjects";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Router>
+       <Navbar>
+        </Navbar>
+            <Routes>
+                <Route path="/" element={<Navigate to="/Home" />} />
+                <Route exact path="/Home" element={<Home />} />
+                <Route path="/About" element={<About />} />
+                <Route path="/Contact" element={<Contact />}
+                />
+            </Routes>
+        </Router>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Components/Navbar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink as Link } from "react-router-dom";
+//import { FaBars } from "react-icons/fa";
+ 
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+            <NavLink to="/Home" activeStyle>
+                        Home
+                    </NavLink>
+                    <NavLink to="/About" activeStyle>
+                        About
+                    </NavLink>
+                    <NavLink to="/Contact" activeStyle>
+                        Contact
+            </NavLink>
+            </Nav>
+        </>
+    );
+};
+ 
+export default Navbar;
+
+const Nav = styled.nav`
+    background: #ac9cff;
+    height: 85px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0.2rem calc((100vw - 1000px) / 2);
+    z-index: 12;
+`;
+
+const NavLink = styled(Link)`
+    color: white;
+    display: flex;
+    align-items: center;
+    font-size: calc(5px + 2vmin);
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    &.active {
+        color: #423874;
+    }
+`;
