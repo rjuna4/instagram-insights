e575f6ef7fad2043b81c96c51c668a0112effb0d
rjunagadhwala <rjunagadhwala@luc.edu>
2024-06-11 15:39:39 -0500

    Added LikedPosts.js and PostsViewed.js

--- src/App.js
+++ src/App.js
@@ -26,16 +26,6 @@ function App() {
                 />
             </Routes>
         </Router>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a> */}
       </header>
     </div>
   );
--- src/Components/PostsViewed.js
+++ src/Components/PostsViewed.js
@@ -1,13 +1,15 @@
 import React from "react";
 import following from "../Data/following.json"
-import liked_posts from "../Data/liked_posts.json"
+import posts_viewed from "../Data/posts_viewed.json"
 import { useState, useEffect } from "react";
 import { Bar } from "react-chartjs-2";
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
-const LikedPosts = () => {
+
+const PostsViewed = () => {
     const [monthlyData, setMonthlyData] = useState([]);
+    const [authorsData, setAuthorsData] = useState({});
 
     useEffect(() => {
         const processData = () => {
@@ -15,25 +17,28 @@ const LikedPosts = () => {
                 item.string_list_data.map(account => account.value)
             ));
 
+            // const authors = {};
             const counts = {};
             const now = new Date();
-            const oneYearAgo = new Date(now.getFullYear() - 1, now.getMonth() + 1);
+            const oneYearAgo = new Date(now.getFullYear() - 1, now.getMonth(), 1);
 
-            liked_posts.likes_media_likes.forEach(likedPost => {
-                likedPost.string_list_data.forEach(likeData => {
-                    const date = new Date(likeData.timestamp * 1000);
+            posts_viewed.impressions_history_posts_seen.forEach(postViewed => {
+                const author = postViewed.string_map_data.Author.value;
+                const timestamp = postViewed.string_map_data.Time.timestamp;    
+                const date = new Date(timestamp * 1000);
                     if (date > oneYearAgo) {
                         const monthYear = `${date.getMonth() + 1}-${date.getFullYear()}`;
                         if (!counts[monthYear]) {
                             counts[monthYear] = { followed: 0, notFollowed: 0 };
+                            // authors[monthYear] = [];
                         }
-                        if (followingAccounts.has(likedPost.title)) {
+                        if (followingAccounts.has(author)) {
                             counts[monthYear].followed++;
                         } else {
                             counts[monthYear].notFollowed++;
                         }
+                        // authors[monthYear].push(author);
                     }
-                });
             });
 
             const sortedCounts = Object.keys(counts)
@@ -45,6 +50,7 @@ const LikedPosts = () => {
                 .map(key => ({ monthYear: key, ...counts[key] }));
 
             setMonthlyData(sortedCounts);
+            // setAuthorsData(authors);
         };
 
         processData();
@@ -54,14 +60,14 @@ const LikedPosts = () => {
         labels: monthlyData.map(item => item.monthYear),
         datasets: [
             {
-                label: 'Liked Posts from Followed Accounts',
+                label: 'Viewed Posts from Followed Accounts',
                 data: monthlyData.map(item => item.followed),
                 backgroundColor: 'rgba(75, 192, 192, 0.6)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1,
             },
             {
-                label: 'Liked Posts from Non-Followed Accounts',
+                label: 'Viewed Posts from Non-Followed Accounts',
                 data: monthlyData.map(item => item.notFollowed),
                 backgroundColor: 'rgba(255, 99, 132, 0.6)',
                 borderColor: 'rgba(255, 99, 132, 1)',
@@ -69,19 +75,17 @@ const LikedPosts = () => {
             }
         ]
     };
-
-
-
-    return(
+    
+    return (
         <div>
-            <h1>Instagram Posts Liked Over the Past Year</h1>
+            <h1>Instagram Posts Viewed In May 2024</h1>
             <Bar data={chartData}/>
         </div>
     );
 };
 
-export default LikedPosts;
+export default PostsViewed;
 
 // const barStyle = {
-//     height: '100px',
+//     margin: '100px',
 //   };
